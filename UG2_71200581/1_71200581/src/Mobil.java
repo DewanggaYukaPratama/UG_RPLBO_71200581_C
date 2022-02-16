de53560f857585ff3fd45de5efc01cdf6c9abc9c
Dewangga Yuka P <dewangga.yuka@ti.ukdw.ac.id>
2022-02-16 17:41:44 +0700

    Add Folder

--- UG2_71200581/1_71200581/src/Mobil.java
+++ UG2_71200581/1_71200581/src/Mobil.java
@@ -1,2 +1,13 @@
-package PACKAGE_NAME;public class Mobil {
+public class Mobil {
+    private String jenis;
+    private String mesin;
+    public void jalan(){
+        System.out.println("Mobil Ngeng...");
+    }
+    public void kurangiKecepatan(){
+        System.out.println("Mobil menguragi kecepatan...");
+    }
+    public void berhenti(){
+        System.out.println("Mobil stop...");
+    }
 }
--- UG2_71200581/1_71200581/src/PejalanKaki.java
+++ UG2_71200581/1_71200581/src/PejalanKaki.java
@@ -1,2 +1,13 @@
-package PACKAGE_NAME;public class PejalanKaki {
+public class PejalanKaki {
+    private String nama;
+    public void menyebrang(){
+        System.out.println("Pejalan kaki melewati zebra cross...");
+    }
+
+    public  void bersiap(){
+        System.out.println("Pejalan kaki bersiap menyebrang...");
+    }
+    public void menunggu(){
+        System.out.println("Pejalan kaki menunggu...");
+    }
 }
