96d45e2330b5ced486b9dff76a9292123ca17dcf
Dewangga Yuka P <dewangga.yuka@ti.ukdw.ac.id>
2022-05-11 18:41:00 +0700

    last commit

---
+++ UG10A_71200581/src/main/java/org/ug10a/User.java
@@ -0,0 +1,14 @@
+package org.ug10a;
+
+public class User {
+    private String username;
+    private String email;
+    private String password;
+
+    public User(String Username, String Email, String Password){
+        username = Username;
+        email = Email;
+        password = Password;
+    }
+
+}
