8e7ea1138a496baf72dd3624c847031672963201
Dewangga Yuka P <dewangga.yuka@ti.ukdw.ac.id>
2022-05-25 18:38:37 +0700

    last commit

---
+++ UG12_71200581/UG12-71200581-ProjectKassa/src/main/java/com/ug12/projectkassa/Kassa.java
@@ -0,0 +1,12 @@
+package com.ug12.projectkassa;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Kassa {
+    private HashMap<Produk, Integer> pesanan;
+    private ArrayList<Kasir> arrKasir;
+    private Kasir kasir;
+
+    public void login(String username, String password)
+}
